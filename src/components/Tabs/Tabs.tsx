32cd87be41c76905820375c1d030f235a75e5a9c
rgsk <guptarahul70322@gmail.com>
2023-08-25 18:18:48 +0530

    tabs spacing

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -9,15 +9,19 @@ const allTabs: TTab[] = ["home", "search", "profile"];
 const Tabs: React.FC<ITabsProps> = ({}) => {
   const pathName = usePathname();
   return (
-    <div className="flex justify-around items-center">
-      {allTabs.map((tab) => {
-        const tabPathName = tab === "home" ? "/" : `/${tab}`;
-        return (
-          <Link key={tab} href={tabPathName}>
-            <TabItem type={tab} active={tabPathName === pathName} />
-          </Link>
-        );
-      })}
+    <div className="py-3 border-t border-t-gray-dark">
+      <div className="py-2">
+        <div className="flex justify-around items-center">
+          {allTabs.map((tab) => {
+            const tabPathName = tab === "home" ? "/" : `/${tab}`;
+            return (
+              <Link key={tab} href={tabPathName}>
+                <TabItem type={tab} active={tabPathName === pathName} />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
